e037a3d06978559b3427ce7f6804b6d75b07074c
Инапшба Шаур Русланович <sh.inapshba@dev.vtb.ru>
2026-09-23 13:57:55 +0300

    fix info message text+1

--- wrapper/functions.py
+++ wrapper/functions.py
@@ -314,6 +314,9 @@ class MeetingFunctions:
             meeting = m_meeting_weekly_double.select_meeting_by_id(meeting_id.replace('_doubleweekly', ''))
         else:
             meeting = m_meeting.select_meeting_by_id(meeting_id)
+
+        if meeting.chat_title == 'Личная':
+            meeting.chat_title = 'Личные сообщения'
         text = f'Информация о встрече ℹ️\n\nЧат: {meeting.chat_title}\n' + meeting.get_view_full()
         self.bot.send_message(message.chat.id, text, parse_mode='HTML', reply_markup=menu)
 
